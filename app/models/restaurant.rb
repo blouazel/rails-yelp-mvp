503e85bd19acf886f2e9967fc97b1efa2d2688c6
briac louazel <briac@laposte.net>
2020-11-11 16:01:03 +0100

    add action create new restaurant

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,4 +3,3 @@ class Restaurant < ApplicationRecord
   validates :name, :address, :category, presence: true
   validates :category, inclusion: { in: ['chinese', 'italian', 'japanese', 'french', 'belgian'] }
 end
-
